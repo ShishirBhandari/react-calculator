e3269cc31e0048e98754db10216ea5b9ef7e0b4f
ShishirBhandari <shishirbhandari54@gmail.com>
2020-07-15 21:22:59 +0545

    Updates

---
+++ precache-manifest.dc53c10476bcc730afb41d6037fbcfd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbbb7f2299116f6d4ef4661ce3949b1f",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "3d061b2e9c2670e8452d",
+    "url": "/react-calculator/static/css/main.dc2bab62.chunk.css"
+  },
+  {
+    "revision": "7a96cfc45877c2654ccb",
+    "url": "/react-calculator/static/js/2.944d1f6e.chunk.js"
+  },
+  {
+    "revision": "3d061b2e9c2670e8452d",
+    "url": "/react-calculator/static/js/main.8d454610.chunk.js"
+  },
+  {
+    "revision": "4f1e8ed48634a9b165a2",
+    "url": "/react-calculator/static/js/runtime-main.95d4c101.js"
+  }
+]);
